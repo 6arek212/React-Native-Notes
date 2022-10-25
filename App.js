5659cd37388643246bcfdec42d6562ac7e87210b
tarikhusin <tarik.id.10@hotmail.com>
2022-10-25 22:13:26 +0300

    init

--- App.js
+++ App.js
@@ -4,17 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello world</Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text>lorem ipsum dolor sit amet.</Text>
+        <Text>lorem ipsum dolor sit amet.</Text>
+        <Text>lorem ipsum dolor sit amet.</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+
+  boldText: {
+    fontWeight: 'bold'
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
